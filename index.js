b3b0851f13f5993295c46e6870c51c6c18b4abc1
asad1660 <asadk1660@gmail.com>
2024-02-14 21:39:57 +0500

    messari api setup with socket io

--- index.js
+++ index.js
@@ -1,13 +1,53 @@
-const PORT= 3000;
+require("dotenv").config();
+const PORT = process.env.PORT;
 const express = require("express");
 const socketIO = require("socket.io");
+const axios = require("axios");
 
 const server = express().listen(PORT, () => {
-    console.log(`listening to the port ${PORT}`)
-})
+  console.log(`Listining to ${PORT}`);
+});
 
 const socketHandler = socketIO(server);
 
-socketHandler.on("connection", () => {
-    console.log("Client Connected")
-})
+socketHandler.on("connection", (socket) => {
+  socket.on("connect_error", () => {
+    console.log("Connection error!");
+  });
+  socket.on("disconnect", () => {
+    console.log("Client disconnected!");
+  });
+  console.log("Client Connected!");
+  socket.emit("crypto", "Hello Cryptos Client!");
+});
+
+const getPrices = () => {
+  axios
+    .get(process.env.LIST_URL, {
+      headers: {
+        "x-messari-api-key": process.env.API_KEY,
+      },
+    })
+    .then((response) => {
+      const priceList = response.data.data.map((item) => {
+        return {
+          id: item.id,
+          name: item.symbol,
+          price: item.metrics.market_data.price_usd,
+        };
+      });
+
+      socketHandler.emit("crypto", priceList);
+    })
+    .catch((err) => {
+      console.log(err);
+      socketHandler.emit("crypto", {
+        error: true,
+        message: "Error Fetching Prices Data From API",
+      });
+    });
+};
+
+setInterval(() => {
+  getPrices();
+}, 20000);
