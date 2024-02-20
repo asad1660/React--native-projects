aed80ae7e18c4c89b4b4f8b4109fa5581573d36a
asad1660 <asadk1660@gmail.com>
2024-02-21 03:44:02 +0500

    cors error

--- index.js
+++ index.js
@@ -6,6 +6,12 @@ const axios = require("axios");
 
 const app = express();
 app.use(express.json());
+app.use((req,res,next)=>{
+  res.setHeader("Access-Control-Allow-Origin","*")
+  res.setHeader("Access-Control-Allow-Headers","Origin, X-Requested-with, Content-Type,Accept,Authorization")
+  res.setHeader("Access-Control-Allow-Methods","GET, POST, PATCH, DELETE, OPTIONS,PUT ")
+  next();
+}) 
 
 const server = app.listen(PORT, () => {
   console.log(`Listining to ${PORT}`);
